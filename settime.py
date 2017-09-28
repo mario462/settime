29d0a0139bd0abd3d82c813aa1cb77ab62fc7bab
Mario Muniz <mario462@gmail.com>
2017-09-28 12:55:10 -0400

    Added README and LICENSE

--- settime.py
+++ settime.py
@@ -1,13 +1,4 @@
-# This script connects to a datetime API in order to set system datetime automatically.
-# A personal API key is used to fetch current datetime and it is set to get datetime for Cuba.
-# Modifications can be made in order to get datetime for other zones. A full list of available zones can be found in https://timezonedb.com/time-zones
-# Please consider registering for your own API key in https://timezonedb.com/register
-# In order to change the system date in Linux, sudo privileges are required.
-# To prevent the script from asking your password when executing `sudo date`, following line must be added to /etc/sudoers file:
-# <user>    ALL=(ALL)    NOPASSWD:   /bin/date
-# Remember to use `sudo visudo` to edit sudoers file.
-# Also, consider executing this script every time at reboot, placing it in a given directory and adding a crontab with `crontab -e`
-# @reboot python3 <script directory> &
+
 
 # Author: Mario Muniz <mario462@gmail.com>
 
